37c34372a8a59989e60d2f6fc3dca6bb6df13140
kgkaye <70980732+kgkaye@users.noreply.github.com>
2021-09-29 12:27:44 -0700

    Creates basic html structure and adds form elements to calculator.php

---
+++ Calculator.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en>">
+<head>
+    <!-- 
+    Graham Kaye
+    September 29
+    DGL 123
+    Week 4 assignment
+    -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="device-width, initial-scale=1">
+    <title>Addition Only Calculator</title>
+    
+</head>
+<body>
+    <h1>Addition Only Calculator</h1>
+
+    <form >
+        <div>
+            <label for="first_number">1st Number</label>
+            <input type="number" name="first_number" value="0"> <!--Adds numeric value to first_number-->
+        </div>
+        <br>
+        <div>
+            <label for="second_number">2nd Number</label>
+            <input type="number" name="second_number" value="0">
+        </div>
+        <br>
+        <input type="submit" value="Calculate Sum">
+    </form>
+</body>
+</html>
